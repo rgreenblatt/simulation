98f7dbc92aa3ab7a7017e307015a9d181ea8df8e
rgreenblatt <greenblattryan@gmail.com>
2020-03-06 19:23:50 -0500

    Building

--- src/display.rs
+++ src/display.rs
@@ -1,8 +1,8 @@
 use crate::Scene;
 use kiss3d::window::Window;
 
-fn display<S : Scene>(window_name : &str, scene : Scene) {
+fn display<S : Scene>(window_name : &str, scene : &mut S) {
   let mut window = Window::new(window_name);
 
-  scene.init_objects(&mut window.new_group());
+  scene.init_objects(&mut window.add_group());
 }
--- src/scene.rs
+++ src/scene.rs
@@ -1,10 +1,10 @@
 use kiss3d::scene::SceneNode;
-use na::Point3;
+use nalgebra::Point3;
 
 // TODO: have up?
 pub struct CameraInfo {
-  pub eye : Point3,
-  pub at : Point3,
+  pub eye : Point3<f32>,
+  pub at : Point3<f32>,
 }
 
 pub trait Scene {
