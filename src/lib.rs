820fa353b9302d8a42cb0cf8f2c25f1990181303
rgreenblatt <greenblattryan@gmail.com>
2020-03-07 11:00:09 -0500

    Fix lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,10 @@
 pub mod display;
 pub mod scene;
 pub mod simulated_scene;
-pub mod tetrahedron;
 pub mod mesh;
 pub mod ode;
 
 pub use display::display_scene;
 pub use scene::{CameraInfo, Scene, SceneGenerator};
 pub use simulated_scene::{SimulatedScene, SimulatedSceneGenerator};
-pub use tetrahedron::Tetrahedron;
 pub use mesh::{load_mesh, LoadedMesh};
